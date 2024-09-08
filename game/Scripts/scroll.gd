76395e5d84384e5a2c08ffb08cc238c569797a97
Ella Rekow <ellarekow@gmail.com>
2024-09-08 16:30:47 -0500

    Scroll open and close in scene

--- game/Scripts/scroll.gd
+++ game/Scripts/scroll.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var scrollSprite = $Scroll2
+
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT: 
-		get_tree().change_scene_to_file("res://Scenes/Scroll.tscn")
+		scrollSprite.visible = !scrollSprite.visible
